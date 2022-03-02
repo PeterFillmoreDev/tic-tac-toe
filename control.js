0eeefcb7cdfd07ccf00103887d82984f719df780
peterfillmore <peterfillmoredev@gmail.com>
2022-03-02 14:10:03 -0500

    convert spagetti code to obj

--- control.js
+++ control.js
@@ -1,45 +1,83 @@
-let turn = 'X';
+    let ticTacToe = {
 
-let gameBoard = document.querySelector('#game-board');
-let gameplay = [{position: 'a1', mark:''},
-                {position: 'a2', mark:''},
-                {position: 'a3', mark:''},
-                {position: 'b1', mark:''},
-                {position: 'b2', mark:''},
-                {position: 'b3', mark:''},
-                {position: 'c1', mark:''},
-                {position: 'c2', mark:''},
-                {position: 'c3', mark:''},
-            ];
+        mark: 'X',
 
-function render(){
-    gameplay.forEach(square => {
-        const squareHTML = `<div class="square" id="pos${square.position}" data-is-marked="unMarked">${square.mark}</div>`
+        squares: [  
+            {position: 'a1', mark:''},
+            {position: 'a2', mark:''},
+            {position: 'a3', mark:''},
+            {position: 'b1', mark:''},
+            {position: 'b2', mark:''},
+            {position: 'b3', mark:''},
+            {position: 'c1', mark:''},
+            {position: 'c2', mark:''},
+            {position: 'c3', mark:''},
+        ],
 
-        const element = document.createElement('div');
+        init: function(){
+            this.cacheDom();
+            this.render();
+            this.bindEvents();
 
-        element.innerHTML = squareHTML; 
+        },
 
-        element.dataset.isMarked = 'unMarked'; 
+        refresh: function(){
+            this.cacheDom();
+            this.render();
+            this.bindEvents();
+            this.turn.turnChange();
+        },
 
-        gameBoard.appendChild(element.lastChild);
-        
-    });
+        cacheDom: function(){
+            this.gameBoard = document.querySelector('#game-board');
+            this.domSquares = this.gameBoard.querySelectorAll('.square');
+        },
 
-    document.querySelectorAll('.square').forEach(square => {
-        square.addEventListener('click', e => {
-            if(square.dataset.isMarked === 'unMarked'){
-                square.innerHTML = turn;
-                square.dataset.isMarked = 'Marked';
-                if(turn == 'X'){
-                    turn = 'O';
+        render: function() {
+            while (this.gameBoard.firstChild) {
+                this.gameBoard.firstChild.remove()
+            };
+            this.squares.forEach(square => {
+                const template = `<div class="square" id="pos${square.position}">${square.mark}</div>`;
+                const element = document.createElement('div');
+                element.innerHTML = template;
+                this.gameBoard.appendChild(element.lastChild);
+            });
+            this.cacheDom();
+        },
+
+        bindEvents: function(){
+            this.domSquares.forEach(square => {this.squareMarker(square)});
+        },
+
+        squareMarker: function(square){
+            let index = this.squareIndexFinder(square); 
+            if(this.squares[index].mark == ''){
+                square.addEventListener('click', e => { 
+                    this.squares[index].mark = this.turn.turn;
+                    this.refresh();
+                });
+            };
+        },
+
+        turn: { 
+            turn: 'X',
+            turnChange: function(){
+                if(this.turn == 'X'){
+                    this.turn = 'O'
                 }
-                else if(turn == 'O'){
-                    turn = 'X';
-                }
-            };  
-        });
-    });
-};
+                else if(this.turn == 'O'){
+                    this.turn = 'X'
+                };
+            },
+        },
 
-render();
+        squareIndexFinder: function(square){
+            let squareid = square.id;
+            let squarePosition = squareid.substring(3);
+            let index = this.squares.map(squares=> squares.position).indexOf(squarePosition);
+            return(index);
+        },
+
+    };
+    ticTacToe.init();
